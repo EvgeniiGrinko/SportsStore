2c672d448ee3aa4b591640104964f6ea3e585657
Evgenii <jenek0907@mail.ru>
2020-10-26 22:33:30 +0300

    some changes

--- src/data/DataStore.js
+++ src/data/DataStore.js
@@ -1,3 +1,8 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { ShopReducer } from "./ShopReducer";
-export const SportsStoreDataStore = createStore(ShopReducer);
+import { CartReducer } from "./CartReducer";
+import { CommonReducer } from "./CommonReducer";
+import { asyncActions } from "./AsyncMiddleware";
+export const SportsStoreDataStore
+= createStore(CommonReducer(ShopReducer, CartReducer),
+applyMiddleware(asyncActions));
--- src/shop/ShopConnector.js
+++ src/shop/ShopConnector.js
@@ -5,9 +5,11 @@ import { connect } from "react-redux";
 import { loadData } from "../data/ActionCreators";
 import { DataTypes } from "../data/Types";
 import { Shop } from "./Shop";
+import { addToCart, updateCartQuantity, removeFromCart, clearCart } from "../data/CartActionCreators";
+import { CartDetails } from "./CartDetails";
 
 const mapStateToProps = (dataStore) => ({...dataStore })
-const mapDispatchToProps = { loadData }
+const mapDispatchToProps = { loadData,addToCart, updateCartQuantity, removeFromCart, clearCart }
 const filterProducts = (products = [], category) =>
         (!category || category === "All")
         ? products
@@ -17,10 +19,12 @@ export const ShopConnector = connect(mapStateToProps, mapDispatchToProps)(
         render() {
             return <Switch>
             <Route path="/shop/products/:category?"
-            render={ (routeProps) =>
-            <Shop { ...this.props } { ...routeProps }
-            products={ filterProducts(this.props.products,
-            routeProps.match.params.category) } />} />
+                render={ (routeProps) =>
+                <Shop { ...this.props } { ...routeProps }
+                products={ filterProducts(this.props.products,
+                routeProps.match.params.category) } />} />
+            <Route path="/shop/cart" render={ (routeProps) =>
+            <CartDetails { ...this.props } { ...routeProps } />} />
             <Redirect to="/shop/products" />
             </Switch>
         }
